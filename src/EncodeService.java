2f9ab04e2170fae90ac75d5615fa5453c134e36f
ShanWenT <98260637+ShanWenT@users.noreply.github.com>
2025-05-03 14:59:54 +0800

    remove partial logic from public
    remove partial logic from public for EncodeService

--- src/EncodeService.java
+++ src/EncodeService.java
@@ -29,40 +29,7 @@ public class EncodeService implements Encoder {
 		System.out.println("Enter offset :");
 		String offset = sc.next().toUpperCase();
 
-		if (offset.length() == 1) {
-			offSetChar = offset.charAt(0);
-//			System.out.println(offSetChar);
-			encodeText.insert(0, offSetChar);
-//			System.out.println(encodeText.toString());
-			offSetInt = referenceTableList.indexOf(offSetChar);
-//			System.out.println(offSetInt);
-			sc.close();
-		} else
-		{
-			Table.invalidOffset=true;
-			sc.close();
-			return "Invalid OffSet";
-		}
-			
-
-		for (int i = 0; i < plainText.length(); i++) {
-			encodeKeys.add(i, referenceTableList.indexOf(plainText.charAt(i)));
-//			System.out.println(encodeKeys);
-		}
-
-		for (int j = 1; j <= encodeKeys.size(); j++) {
-
-//			System.out.println(encodeKeys.get(j-1));
-
-			if (encodeKeys.get(j - 1) == -1)
-				encodeText.insert(j, " ");
-			else if (encodeKeys.get(j - 1) - offSetInt < 0)
-				encodeText.insert(j,
-						referenceTableList.get(encodeKeys.get(j - 1) - offSetInt + referenceTableList.size()));
-
-			else
-				encodeText.insert(j, referenceTableList.get(encodeKeys.get(j - 1) - offSetInt));
-		}
+		//Logic removed to public - Shan Wen
 
 		return encodeText.toString();
 	}
